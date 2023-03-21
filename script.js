e710a1b1f2ce2c5bc41663132e74a6292f11c98d
Gabe Lee <gibgab6@gmail.com>
2023-03-21 16:15:23 -0400

    got the buttons and everything working now spacing

--- script.js
+++ script.js
@@ -1,16 +1,18 @@
-// Get the like button element
-var likeBtn = document.getElementById("likebtn");
+// Get the like buttons elements
+var likeBtns = document.querySelectorAll("#likebtn1, #likebtn2, #likebtn3");
 
-// Get the likes count element
-var likesCount = document.querySelector(".likes");
+// Get the likes counts elements
+var likesCounts = document.querySelectorAll(".likes");
 
-// Set the initial likes count to 3
-var count = 3;
+// Set the initial likes count to 3 for each post
+var counts = [9, 12, 9];
 
-// Add a click event listener to the like button
-likeBtn.addEventListener("click", function () {
-    // Increase the count by 1
-    count++;
-    // Update the likes count text
-    likesCount.textContent = count + " like(s)";
+// Add a click event listener to each like button
+likeBtns.forEach(function (likeBtn, i) {
+    likeBtn.addEventListener("click", function () {
+        // Increase the count by 1
+        counts[i]++;
+        // Update the likes count text
+        likesCounts[i].textContent = counts[i] + " like(s)";
+    });
 });
